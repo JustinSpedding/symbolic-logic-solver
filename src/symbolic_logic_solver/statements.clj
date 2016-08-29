2653624e4d073a06b2e4d386aef585f3b85bfb87
Justin Spedding <justinspedding@users.noreply.github.com>
2016-08-28 20:32:33 -0400

    Implemented consistency check

--- src/symbolic_logic_solver/statements.clj
+++ src/symbolic_logic_solver/statements.clj
@@ -1,20 +1,49 @@
-(ns symbolic-logic-solver.statements)
+(ns symbolic-logic-solver.statements
+  (:require [clojure.set :use [instersection]]))
 
 (defrecord Var [var])
-
 (defrecord And [arg1 arg2])
-
 (defrecord Or [arg1 arg2])
-
 (defrecord Equ [arg1 arg2])
-
 (defrecord Ent [arg1 arg2])
-
 (defrecord Not [arg1])
+
+(defn Var? [statement] (= Var (type statement)))
+(defn And? [statement] (= And (type statement)))
+(defn Or? [statement] (= Or (type statement)))
+(defn Equ? [statement] (= Equ (type statement)))
+(defn Ent? [statement] (= Ent (type statement)))
+(defn Not? [statement] (= Not (type statement)))
 
 (defn atom? [statement]
   (or (= Var (type statement))
       (and (= Not (type statement))
            (= Var (type (:arg1 statement))))))
 
-(defn consistent? [& statements])
+(defn- consistent?-worker [statements true-vars false-vars]
+  (loop [statements statements
+         true-vars true-vars
+         false-vars false-vars]
+    (if (empty? (clojure.set/intersection true-vars false-vars))
+      (if-let [statement (first statements)]
+        (cond (Var? statement) (recur (rest statements) (conj true-vars statement) false-vars)
+              (And? statement) (recur (conj (rest statements) (:arg1 statement) (:arg2 statement)) true-vars false-vars)
+              (Or? statement)  (or (consistent?-worker (conj (rest statements) (:arg1 statement)) true-vars false-vars)
+                                   (consistent?-worker (conj (rest statements) (:arg2 statement)) true-vars false-vars))
+              (Equ? statement) (or (consistent?-worker (conj (rest statements) (:arg1 statement) (:arg2 statement)) true-vars false-vars)
+                                   (consistent?-worker (conj (rest statements) (->Not (:arg1 statement)) (->Not (:arg2 statement))) true-vars false-vars))
+              (Ent? statement) (or (consistent?-worker (conj (rest statements) (->Not (:arg1 statement))) true-vars false-vars)
+                                   (consistent?-worker (conj (rest statements) (:arg2 statement)) true-vars false-vars))
+              (Not? statement) (let [inner-statement (:arg1 statement)]
+                                 (cond (Var? inner-statement) (recur (rest statements) true-vars (conj false-vars inner-statement))
+                                       (And? inner-statement) (or (consistent?-worker (conj (rest statements) (->Not (:arg1 inner-statement))) true-vars false-vars)
+                                                                  (consistent?-worker (conj (rest statements) (->Not (:arg2 inner-statement))) true-vars false-vars))
+                                       (Or? inner-statement)  (recur (conj (rest statements) (->Not (:arg1 inner-statement)) (->Not (:arg2 inner-statement))) true-vars false-vars)
+                                       (Equ? inner-statement) (or (consistent?-worker (conj (rest statements) (:arg1 inner-statement) (->Not (:arg2 inner-statement))) true-vars false-vars)
+                                                                  (consistent?-worker (conj (rest statements) (->Not (:arg1 inner-statement)) (:arg2 inner-statement)) true-vars false-vars))
+                                       (Ent? inner-statement) (recur (conj (rest statements) (:arg1 inner-statement) (->Not (:arg2 inner-statement))) true-vars false-vars)
+                                       (Not? inner-statement) (recur (conj (rest statements) (:arg1 inner-statement)) true-vars false-vars))))
+        true))))
+
+(defn consistent? [& statements]
+  (consistent?-worker statements #{} #{}))
--- test/symbolic_logic_solver/statements_test.clj
+++ test/symbolic_logic_solver/statements_test.clj
@@ -12,3 +12,105 @@
     (is (not (atom? (->Equ (->Var \p) (->Var \q)))))
     (is (not (atom? (->Ent (->Var \p) (->Var \q)))))
     (is (not (atom? (->Not (->Not (->Var \p))))))))
+
+(deftest consistent?-test
+  (testing "corrently identifies consistent groups of statements"
+    (is (consistent?))
+
+    (is (consistent? (->Var \p)))
+
+    (is (consistent? (->Var \p)
+                     (->Var \q)))
+
+    (is (consistent? (->Var \p)
+                     (->Var \q)
+                     (->Not (->Var \r))))
+
+    (is (consistent? (->Var \p)
+                     (->Var \q)
+                     (->Not (->Var \r))
+                     (->Not (->Var \s))))
+
+    (is (consistent? (->Var \p)
+                     (->Var \p)))
+
+    (is (consistent? (->Not (->Var \p))
+                     (->Not (->Var \q))))
+
+    (is (consistent? (->And (->Var \p)
+                            (->Var \q))
+                     (->Var \p)
+                     (->Var \q)))
+
+    (is (consistent? (->Or (->Var \p)
+                           (->Var \q))
+                     (->Var \p)
+                     (->Var \q)))
+
+    (is (consistent? (->Equ (->Var \p)
+                            (->Var \q))
+                     (->Var \p)
+                     (->Var \q)))
+
+    (is (consistent? (->Ent (->Var \p)
+                            (->Var \q))
+                     (->Var \p)
+                     (->Var \q))))
+
+  (testing "corrently identifies inconsistent groups of statements"
+    (is (not (consistent? (->Var \p)
+                          (->Not (->Var \p)))))
+
+    (is (not (consistent? (->And (->Var \p)
+                                 (->Var \q))
+                          (->Not (->Var \p)))))
+
+    (is (not (consistent? (->And (->Var \p)
+                                 (->Var \q))
+                          (->Not (->Var \q)))))
+
+    (is (not (consistent? (->Or (->Var \p)
+                                (->Var \q))
+                          (->Not (->Var \p))
+                          (->Not (->Var \q)))))
+
+    (is (not (consistent? (->Equ (->Var \p)
+                                 (->Var \q))
+                          (->Var \p)
+                          (->Not (->Var \q)))))
+
+    (is (not (consistent? (->Ent (->Var \p)
+                                 (->Var \q))
+                          (->Var \p)
+                          (->Not (->Var \q)))))
+
+    (is (not (consistent? (->Not (->And (->Var \p)
+                                        (->Var \q)))
+                          (->Var \p)
+                          (->Var \q))))
+
+    (is (not (consistent? (->Not (->Or (->Var \p)
+                                       (->Var \q)))
+                          (->Var \p))))
+
+    (is (not (consistent? (->Not (->Or (->Var \p)
+                                       (->Var \q)))
+                          (->Var \q))))
+
+    (is (not (consistent? (->Not (->Equ (->Var \p)
+                                        (->Var \q)))
+                          (->Var \p)
+                          (->Var \q))))
+
+    (is (not (consistent? (->Not (->Equ (->Var \p)
+                                        (->Var \q)))
+                          (->Not (->Var \p))
+                          (->Not (->Var \q)))))
+
+    (is (not (consistent? (->Not (->Ent (->Var \p)
+                                        (->Var \q)))
+                          (->Not (->Var \p)))))
+
+    (is (not (consistent? (->Not (->Ent (->Var \p)
+                                        (->Var \q)))
+                          (->Var \q))))))
