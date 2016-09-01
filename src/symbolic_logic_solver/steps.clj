6c1d43027457e9e9765d6a5cb2482a201e0010d6
Justin Spedding <justinspedding@users.noreply.github.com>
2016-09-01 09:13:57 -0400

    Made the step definitions a bit more readable

--- src/symbolic_logic_solver/steps.clj
+++ src/symbolic_logic_solver/steps.clj
@@ -1,17 +1,17 @@
 (ns symbolic-logic-solver.steps)
 
 (defrecord AndElimination [arg1 conclusion])
-(defrecord OrElimination [arg1 arg2 arg3 conclusion])
+(defrecord OrElimination  [arg1 arg2 arg3 conclusion])
 (defrecord EquElimination [arg1 arg2 conclusion])
 (defrecord EntElimination [arg1 arg2 conclusion])
 (defrecord NotElimination [arg1 conclusion])
 
 (defrecord AndIntroduction [arg1 arg2 conclusion])
-(defrecord OrIntroduction [arg1 conclusion])
+(defrecord OrIntroduction  [arg1 conclusion])
 (defrecord EquIntroduction [arg1 arg2 conclusion])
 (defrecord EntIntroduction [arg1 conclusion])
 (defrecord NotIntroduction [arg1 conclusion])
 
 (defrecord Reiteration [conclusion])
 
-(defrecord Assumption [assumption steps])
+(defrecord Assumption [assumption conclusion])
