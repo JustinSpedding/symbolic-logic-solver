58a688705aa5f34099eaa4a62aed78610ecde949
Justin Spedding <justinspedding@users.noreply.github.com>
2016-09-04 13:46:56 -0400

    Connected parser, generator, and formatter in core

--- src/symbolic_logic_solver/core.clj
+++ src/symbolic_logic_solver/core.clj
@@ -1,7 +1,17 @@
 (ns symbolic-logic-solver.core
-  (:gen-class))
+  (:gen-class)
+  (:require [symbolic-logic-solver.statement-parser :as parser]
+            [symbolic-logic-solver.proof-generator :as generator]
+            [symbolic-logic-solver.proof-formatter :as formatter]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn prove [assumptions conclusion]
+  (let [assumptions (map parser/string-to-statement assumptions)
+        conclusion (parser/string-to-statement conclusion)]
+    (if (generator/entails? assumptions conclusion)
+      (formatter/format-proof (generator/generate-proof assumptions conclusion))
+      "The assumptions do not entail the conclusion.")))
--- src/symbolic_logic_solver/proof_formatter.clj
+++ src/symbolic_logic_solver/proof_formatter.clj
@@ -1,3 +1,5 @@
 (ns symbolic-logic-solver.proof-formatter
   (:require [symbolic-logic-solver.statements :refer :all]
             [symbolic-logic-solver.steps :refer :all]))
+
+(defn format-proof [proof])
