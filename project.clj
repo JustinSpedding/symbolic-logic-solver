5fee6224af71201de0ab5ff4b056eef5af691ba3
Justin Spedding <justinspedding@users.noreply.github.com>
2016-10-10 21:42:36 -0400

    Added nrepl to dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.nrepl "0.2.12"]]
   :main ^:skip-aot symbolic-logic-solver.core
   :target-path "target/%s"
   :plugins [[refactor-nrepl "1.1.0"]
